7c15b15a50682794f439d10caf4ceb13df8393c9
varun-reddy09 <43644332+varun-reddy09@users.noreply.github.com>
2019-03-13 10:53:53 +0530

    Update urls.py

--- restapi/urls.py
+++ restapi/urls.py
@@ -12,9 +12,15 @@ urlpatterns= [
     #path('get/details',views.showDetails, name = 'showDetails'),
     #path('get/registrations',views.showRegistrationsAndParticipations, name = 'showRegistrationsAndParticipations'),
     #path('get/event', views.showEvent, name = 'showEvent'),
+    
+    #this is to add a new game
     path('events/newgame',views.newGame, name = 'newGame'),
+    #this is to append players to an existing team
     path('events/addplayer',views.appendPlayers, name = 'appendPlayer'),
+    #this is to update status of game to played and update the points
     path('events/endgame',views.endGame, name = 'endGame'),
+    #this is to fetch both the played and not played games which are registered by user
     path('register/fetch',views.getUserEvent, name = 'getUserEvent'),
+    #this is to modify the event details with paid and payment mode details
     path('register/push',views.modifyRegistrationsAndParticipations, name = 'modifyRegistrationsAndParticipations'),
 ]
