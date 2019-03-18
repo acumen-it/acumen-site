5feecdf001cc60d3d5379d37d1609b5c63a40bde
Krushi Raj Tula <krushiraj123@gmail.com>
2019-03-18 21:48:48 +0530

    Update admin.py

--- acusite/admin.py
+++ acusite/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import Profile,Event,EventDetails,Organizer,Team,Otpgenerator
-admin.site.register(Profile)
-admin.site.register(Organizer)
-admin.site.register(Event)
-admin.site.register(EventDetails)
-admin.site.register(Otpgenerator)
+
+@admin.register(Profile)
+class ProfileAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(Organizer)
+class OrganizerAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(Event)
+class EventAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(EventDetails)
+class EventDetailsAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(Otpgenerator)
+class OtpgeneratorAdmin(ImportExportModelAdmin):
+    pass
+
 
 # Register your models here.
