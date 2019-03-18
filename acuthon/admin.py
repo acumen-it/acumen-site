bcb44e8780848407b26ae703c6c3cf13d5ac94fa
Krushi Raj Tula <krushiraj123@gmail.com>
2019-03-18 21:50:02 +0530

    Update admin.py

--- acuthon/admin.py
+++ acuthon/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-
-
+from import_export.admin import ImportExportModelAdmin
 from .models import *
+
 # Register your models here.
-admin.site.register(Participant)
-admin.site.register(Team)
-admin.site.register(Payment)
+@admin.register(Participant)
+class ParticipantAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(Team)
+class TeamAdmin(ImportExportModelAdmin):
+    pass
+
+@admin.register(Payment)
+class PaymentAdmin(ImportExportModelAdmin):
+    pass
